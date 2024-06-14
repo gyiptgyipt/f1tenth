57315563a6842326f79660c034e48e16acab4526
Htet-Wai-Yan-HWY <htetwaiyan.zyme@gmail.com>
2024-06-15 01:01:44 +0630

    added_nextpoint_vizer

--- src/waypoint_visualizer/src/next_point_visualizer.cpp
+++ src/waypoint_visualizer/src/next_point_visualizer.cpp
@@ -4,19 +4,15 @@
 #include <std_msgs/msg/color_rgba.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <nav_msgs/msg/path.hpp>
-#include <math.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <cmath>
-#include <limit.hpp>
-
 #include <chrono>
 #include <cstdio>
 #include <memory>
 #include <string>
 
 using namespace std::chrono_literals;
-using std::placeholders::_1;
 
 class PointSpherePublisher : public rclcpp::Node
 {
@@ -25,41 +21,31 @@ public:
     : Node("point_sphere_publisher")
     {
         publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("/visualization_marker", 10);
-        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&PointSpherePublisher::get_next_point, this, _1));
-        waypoint_sub_ = this->create_subscription<visualization_msgs::msg::Marker>("/centerline_waypoints",10,std::bind(&PointSpherePublisher::update_waypoint,this,_1));
+        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&PointSpherePublisher::get_next_point, this,std::placeholders::_1));
+        waypoint_sub_ = this->create_subscription<nav_msgs::msg::Path>("/centerline_waypoints", 10, std::bind(&PointSpherePublisher::update_waypoint, this,std::placeholders::_1));
 
         timer_ = this->create_wall_timer(500ms, std::bind(&PointSpherePublisher::timerCallback, this));
-        
     }
 
 private:
-
     std::vector<geometry_msgs::msg::Point> points;
-
-    double car_pos_x , car_pos_y , car_yaw;
-    double point_pos_x, point_pos_y;
-
-    double closest_distance = NULL;
+    double car_pos_x, car_pos_y, car_yaw;
+    double closest_distance = std::numeric_limits<double>::max();
     std::vector<geometry_msgs::msg::Point> closest_waypoint;
 
-    void update_waypoint(nav_msgs::msg::Path::SharedPtr msg){
-
-        
-        for (const auto& pose : msg->poses) {
+    void update_waypoint(const nav_msgs::msg::Path::SharedPtr msg)
+    {
+        for (const auto &pose : msg->poses) {
             geometry_msgs::msg::Point point;
             point.x = pose.pose.position.x;
             point.y = pose.pose.position.y;
             point.z = pose.pose.position.z;
             this->points.push_back(point);
         }
-
     }
 
-
-    void get_next_point(const nav_msgs::msg::Odometry::ConstSharedPtr odom_msg){
-        
-        double dist = 0.6604;
-
+    void get_next_point(const nav_msgs::msg::Odometry::ConstSharedPtr odom_msg)
+    {
         this->car_pos_x = odom_msg->pose.pose.position.x;
         this->car_pos_y = odom_msg->pose.pose.position.y;
 
@@ -68,89 +54,78 @@ private:
             odom_msg->pose.pose.orientation.y,
             odom_msg->pose.pose.orientation.z,
             odom_msg->pose.pose.orientation.w);
-    
+
         tf2::Matrix3x3 m(quat);
         double roll, pitch, yaw;
         m.getRPY(roll, pitch, yaw);
 
         this->car_yaw = yaw;
-        
     }
 
-    
-    void timerCallback(){
-        double search_angle = 1.5; //85 degree
+    void timerCallback()
+    {
+        double search_angle = 1.5; // 85 degrees
         double car_length = 0.3302;
         double min_dist = car_length * 2.0;
-        
-        for (const auto &point : points){     //getting waypoints 
-            double dx = point.x - this->car_pos_x; 
+        double closest_distance = std::numeric_limits<double>::max();
+        geometry_msgs::msg::Point closest_point;
+
+        for (const auto &point : points) { // getting waypoints
+            double dx = point.x - this->car_pos_x;
             double dy = point.y - this->car_pos_y;
             double angle = atan2(dy, dx);
             double relative_angle = get_relative_angle(car_yaw, angle);
-            double distance = sqrt(pow(dx,2) + pow(dx,2));
+            double distance = sqrt(pow(dx, 2) + pow(dy, 2)); // Use dx and dy
 
-            if (abs(relative_angle) < search_angle && distance < closest_distance && distance > min_dist){
-                    this->closest_distance = distance;
-                    this->closest_waypoint.push_back(point);
+            if (abs(relative_angle) < search_angle && distance < closest_distance && distance > min_dist) {
+                closest_distance = distance;
+                closest_point = point;
             }
+        }
 
-        //Let's viz Point
-        auto marker = visualization_msgs::msg::Marker();
+        // Let's visualize the closest Point
+        visualization_msgs::msg::Marker marker;
         marker.header.frame_id = "map";
         marker.header.stamp = this->now();
-        marker.ns = "";
-        marker.id = 1;
-        marker.type = visualization_msgs::msg::Marker::POINTS;
+        marker.ns = "waypoints";
+        marker.id = 0;
+        marker.type = visualization_msgs::msg::Marker::SPHERE;
         marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.pose.position = closest_point;
         marker.pose.orientation.w = 1.0;
-
-        // Points to visualize
-        
-        geometry_msgs::msg::Point p;
-        p.x = this->closest_waypoint.pose.position.x;
-        p.y = this->closest_waypoint.pose.position.y;
-        p.z = 0.0;
-        marker.points.push_back(p);
-        
-
-        // Set the scale of the points
         marker.scale.x = 0.2;
         marker.scale.y = 0.2;
-
-        // Set the color of the points
-        marker.color.a = 1.0;  // Alpha
+        marker.scale.z = 0.2;
+        marker.color.a = 1.0;
         marker.color.r = 0.0;
         marker.color.g = 1.0;
         marker.color.b = 0.0;
 
         publisher_->publish(marker);
-            
-
-        }
     }
 
-    double angle_check(double angle){
-        while (angle > M_PI){
+    double angle_check(double angle)
+    {
+        while (angle > M_PI) {
             angle -= 2 * M_PI;
-            }
-        while (angle < -M_PI){
+        }
+        while (angle < -M_PI) {
             angle += 2 * M_PI;
-            }
+        }
         return angle;
     }
 
-    double get_relative_angle(double car_yaw,double angle){
+    double get_relative_angle(double car_yaw, double angle)
+    {
         double relative_angle = angle - car_yaw;
         relative_angle = angle_check(relative_angle);
         return relative_angle;
     }
 
-
     rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-    rclcpp::Subscription<visualization_msgs::msg::Marker>::SharedPtr waypoint_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr waypoint_sub_;
 };
 
 int main(int argc, char *argv[])
