638899b9baa5e21f1c4f19a5e1f5681e8a5bd7a2
Htet-Wai-Yan-HWY <htetwaiyan.zyme@gmail.com>
2024-06-14 20:04:52 +0630

    viz_part

--- src/waypoint_visualizer/src/next_point_visualizer.cpp
+++ src/waypoint_visualizer/src/next_point_visualizer.cpp
@@ -7,6 +7,8 @@
 #include <math.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
+#include <cmath>
+#include <limit.hpp>
 
 #include <chrono>
 #include <cstdio>
@@ -32,13 +34,24 @@ public:
 
 private:
 
-    const nav_msgs::msg::Path path_point;
+    std::vector<geometry_msgs::msg::Point> points;
 
+    double car_pos_x , car_pos_y , car_yaw;
     double point_pos_x, point_pos_y;
 
-    void update_waypoint(nav_msgs::msg::Path msg){
+    double closest_distance = NULL;
+    std::vector<geometry_msgs::msg::Point> closest_waypoint;
 
-        // path_point.pose.position.X = 0;
+    void update_waypoint(nav_msgs::msg::Path::SharedPtr msg){
+
+        
+        for (const auto& pose : msg->poses) {
+            geometry_msgs::msg::Point point;
+            point.x = pose.pose.position.x;
+            point.y = pose.pose.position.y;
+            point.z = pose.pose.position.z;
+            this->points.push_back(point);
+        }
 
     }
 
@@ -47,8 +60,8 @@ private:
         
         double dist = 0.6604;
 
-        auto car_pos_x = odom_msg->pose.pose.position.x;
-        auto car_pos_y = odom_msg->pose.pose.position.y;
+        this->car_pos_x = odom_msg->pose.pose.position.x;
+        this->car_pos_y = odom_msg->pose.pose.position.y;
 
         tf2::Quaternion quat(
             odom_msg->pose.pose.orientation.x,
@@ -59,37 +72,78 @@ private:
         tf2::Matrix3x3 m(quat);
         double roll, pitch, yaw;
         m.getRPY(roll, pitch, yaw);
-        
-        
+
+        this->car_yaw = yaw;
         
     }
 
     
-    
     void timerCallback(){
+        double search_angle = 1.5; //85 degree
+        double car_length = 0.3302;
+        double min_dist = car_length * 2.0;
+        
+        for (const auto &point : points){     //getting waypoints 
+            double dx = point.x - this->car_pos_x; 
+            double dy = point.y - this->car_pos_y;
+            double angle = atan2(dy, dx);
+            double relative_angle = get_relative_angle(car_yaw, angle);
+            double distance = sqrt(pow(dx,2) + pow(dx,2));
+
+            if (abs(relative_angle) < search_angle && distance < closest_distance && distance > min_dist){
+                    this->closest_distance = distance;
+                    this->closest_waypoint.push_back(point);
+            }
+
+        //Let's viz Point
         auto marker = visualization_msgs::msg::Marker();
         marker.header.frame_id = "map";
         marker.header.stamp = this->now();
-        marker.ns = "my_namespace";
-        marker.id = 0;
-        marker.type = visualization_msgs::msg::Marker::SPHERE;
+        marker.ns = "";
+        marker.id = 1;
+        marker.type = visualization_msgs::msg::Marker::POINTS;
         marker.action = visualization_msgs::msg::Marker::ADD;
-        marker.pose.position.x = 0.0;
-        marker.pose.position.y = 0.0;
-        marker.pose.position.z = 0.0;
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
         marker.pose.orientation.w = 1.0;
-        marker.scale.x = 1.0;
-        marker.scale.y = 1.0;
-        marker.scale.z = 1.0;
-        marker.color.a = 1.0; // Don't forget to set the alpha!
+
+        // Points to visualize
+        
+        geometry_msgs::msg::Point p;
+        p.x = this->closest_waypoint.pose.position.x;
+        p.y = this->closest_waypoint.pose.position.y;
+        p.z = 0.0;
+        marker.points.push_back(p);
+        
+
+        // Set the scale of the points
+        marker.scale.x = 0.2;
+        marker.scale.y = 0.2;
+
+        // Set the color of the points
+        marker.color.a = 1.0;  // Alpha
         marker.color.r = 0.0;
         marker.color.g = 1.0;
-        // marker.color.h = 0.0;
+        marker.color.b = 0.0;
 
         publisher_->publish(marker);
+            
+
+        }
+    }
+
+    double angle_check(double angle){
+        while (angle > M_PI){
+            angle -= 2 * M_PI;
+            }
+        while (angle < -M_PI){
+            angle += 2 * M_PI;
+            }
+        return angle;
+    }
+
+    double get_relative_angle(double car_yaw,double angle){
+        double relative_angle = angle - car_yaw;
+        relative_angle = angle_check(relative_angle);
+        return relative_angle;
     }
 
 
